e6d390d3e952d24bfa4471a58f726b858b5c5244
Vencislav <vencislav67@hotmail.com>
2022-06-22 20:30:45 +0300

    Add project files.

---
+++ DefiningClasses/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Car
+    {
+        private string model;
+        private 
+    }
+}
